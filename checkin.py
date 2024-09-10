faa10428ee9056c45a4724087c3598544056ae2d
1-OTP <piu9066@gmail.com>
2024-09-11 00:26:48 +0700

    update: updated read me content

--- checkin.py
+++ checkin.py
@@ -5,8 +5,8 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Replace with your actual bot token and chat ID
-TELEGRAM_BOT_TOKEN = "Your bot token"
-TELEGRAM_CHAT_ID = "you chat id"
+TELEGRAM_BOT_TOKEN = "Your token"
+TELEGRAM_CHAT_ID = "you chat ID"
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
